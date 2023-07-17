d5a0ec24fb180669b23744d32b57e33efadd9a12
Crowdbotics <team@crowdbotics.com>
2023-07-17 01:56:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-bird-42221",
+  url: "https://long-bird-42221.botics.co",
+  api: "https://long-bird-42221.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
